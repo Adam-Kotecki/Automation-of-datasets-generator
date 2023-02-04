6e76db413776605bebfb5a5dca17690c6394a1f5
Adam-Kotecki <a.kotecki11@gmail.com>
2023-02-04 16:49:29 +0100

    initial gui window

--- Generating data automation.py
+++ Generating data automation.py
@@ -1,16 +1,45 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import sys
 
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__(parent=None)
+        self.setWindowTitle("Generating data automation")
+        self.setFixedSize(400, 500)
+
+def main():
+    app = QApplication([])
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
+
+
+'''
 service = Service(executable_path='./chromedriver')
 
-chrome_browser = webdriver.Chrome(service=service)
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+# options.add_argument('ignore-certificate-errors')
 
-#chrome_browser = webdriver.Chrome('./chromedriver')
+chrome_browser = webdriver.Chrome(service=service, chrome_options=options)
 
+chrome_browser.maximize_window()
 chrome_browser.get('https://generatedata.com/')
 
-chrome_browser.close()
+name_btn = chrome_browser.find_element(By.XPATH, '//div[text()="Name"]')
+name_btn.click()
 
+name_btn = chrome_browser.find_element(By.XPATH, '//div[text()="Country"]')
+name_btn.click()
+
+#chrome_browser.close()
+'''
 
 
 
